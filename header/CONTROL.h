791a41652157202384e09502c916c348784a7a1c
mahdiyar-raees <60273385+mahdiyar-raees@users.noreply.github.com>
2023-06-27 12:41:46 +0330

    Add files via upload

---
+++ header/CONTROL.h
@@ -0,0 +1,61 @@
+#ifndef ERROR
+  #define ERROR
+	#define ON 1
+	#define OFF 0
+	
+	
+		//CELL NUMBER
+	#define CELL1   ON
+	#define CELL2   ON
+	#define CELL3   ON
+	#define CELL4   OFF
+	#define CELL5   OFF
+	#define CELL6   OFF
+	#define CELL7   OFF
+	#define CELL8   OFF
+	#define CELL9   OFF
+	#define CELL10  OFF
+	#define CELL11  OFF	
+	#define CELL12  OFF
+	#define CELL13  OFF
+	#define CELL14  OFF
+	#define CELL15  OFF
+	#define CELL16  ON
+	
+	
+	
+	
+	//temperurure check
+	#define ERROR_TEMP                    OFF
+	
+	
+	//charge current check
+	#define CHARGE_CURRENT_ERROR         	ON	
+	
+	
+	//decharge current check
+	#define DECHARGE_CURRENT_ERRPR      	ON
+	
+	
+	//CUV AND COV chack 
+	#define VOLTAGE_ERROR  				       	ON	
+	
+	// BQ fult check
+	#define BQ_CHECK_ERROR                ON
+	
+	// full battery check
+	#define FULL_BATTERY_CHECK            ON
+	
+	//release check
+	#define RELEASE_ERROR_CHECK						ON
+	
+
+
+	#define EMERGANCY_CHECKING						ON
+	
+	#define BQ_FINDER 										ON
+
+	#define TEST													ON
+	
+	#endif
+	
